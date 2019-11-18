275fb3bc350ee993b13a029ffd72d7962353e0b0
huytq27 <57428965+huytq27@users.noreply.github.com>
2019-11-18 07:47:33 +0700

    Chua thay sound

--- src/DAO/DBConnection.java
+++ src/DAO/DBConnection.java
@@ -22,7 +22,7 @@ public class DBConnection {
     private static String DB_URL = "jdbc:sqlserver://Localhost;"
             + "databaseName=data_vituan;";
     private static String USER = "sa";
-    private static String PASSWORD = "songlong";
+    private static String PASSWORD = "admin";
     static Connection conn = null;
 
     public DBConnection() {
